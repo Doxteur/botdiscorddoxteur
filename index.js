e60df93d6f94422d3a0207b20d8a613bf9e635f8
Doxteur <Doxteurn@gmail.com>
2018-06-29 16:04:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const bot = new Discord.Client();
 var prefix = ('*');
 
 bot.on('ready', function() {
+    bot.user.setUsername('HackceS');
     bot.user.setAvatar('./cookie.png')
-    bot.user.setGame('Bz des meres');
+    bot.user.setGame('*ping');
     console.log('Connected');
 
 });
